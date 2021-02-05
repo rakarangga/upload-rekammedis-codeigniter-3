6f3cdbd3812eb9cde6c94fe9dc29f17b2b26a001
rakarangga <9mail.raka@gmail.com>
2021-02-05 11:33:21 +0700

    DELETE rekam_medis.zip & UPDATE LOG ERROR

---
+++ application/logs/log-2021-02-04.php
@@ -0,0 +1,70 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2021-02-04 06:49:56 --> Query error: MySQL server has gone away - Invalid query: SELECT *
+FROM `t_berkas`
+ORDER BY `t_berkas`.`modified` DESC
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2021-02-04 06:49:56 --> Query error: MySQL server has gone away - Invalid query: SELECT SUM(`stts`) AS `stts`
+FROM `t_pasien`
+WHERE `stts` = 1 AND (`created` BETWEEN DATE_ADD(CURDATE(), INTERVAL -1 day) AND CURDATE())
+ORDER BY `t_pasien`.`modified` DESC
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2021-02-04 06:49:56 --> Query error: MySQL server has gone away - Invalid query: SELECT *
+FROM `t_berkas`
+ORDER BY `t_berkas`.`modified` DESC
+ERROR - 2021-02-04 06:49:56 --> Query error: MySQL server has gone away - Invalid query: SELECT SUM(`stts`) AS `stts`
+FROM `t_pasien`
+WHERE DATE(created) = '2021-02-03'
+AND `stts` = 1
+ORDER BY `t_pasien`.`modified` DESC
+ERROR - 2021-02-04 06:49:56 --> Query error: MySQL server has gone away - Invalid query: SELECT *
+FROM `t_pasien`
+WHERE `stts` = 1
+ORDER BY `t_pasien`.`modified` DESC
+ERROR - 2021-02-04 06:49:56 --> Query error: MySQL server has gone away - Invalid query: SELECT *
+FROM `t_pasien`
+WHERE `stts` = 1
+ORDER BY `t_pasien`.`modified` DESC
+ERROR - 2021-02-04 06:49:56 --> Query error: MySQL server has gone away - Invalid query: SELECT SUM(`stts`) AS `stts`
+FROM `t_pasien`
+WHERE DATE(created) = '2021-02-03'
+AND `stts` = 1
+ORDER BY `t_pasien`.`modified` DESC
+ERROR - 2021-02-04 06:49:56 --> Query error: MySQL server has gone away - Invalid query: SELECT SUM(`stts`) AS `stts`
+FROM `t_pasien`
+WHERE `stts` = 1 AND (`created` BETWEEN DATE_ADD(CURDATE(), INTERVAL -1 day) AND CURDATE())
+ORDER BY `t_pasien`.`modified` DESC
+ERROR - 2021-02-04 06:49:56 --> Query error: MySQL server has gone away - Invalid query: SELECT SUM(`stts`) AS `stts`
+FROM `t_pasien`
+WHERE `stts` = 1 AND (`created` BETWEEN DATE_ADD(CURDATE(), INTERVAL -1 day) AND CURDATE())
+ORDER BY `t_pasien`.`modified` DESC
+ERROR - 2021-02-04 06:49:56 --> Query error: MySQL server has gone away - Invalid query: SELECT SUM(`stts`) AS `stts`
+FROM `t_pasien`
+WHERE `stts` = 1 AND (`created` BETWEEN DATE_ADD(CURDATE(), INTERVAL -1 day) AND CURDATE())
+ORDER BY `t_pasien`.`modified` DESC
+ERROR - 2021-02-04 06:49:56 --> Query error: MySQL server has gone away - Invalid query: SELECT SUM(`stts`) AS `stts`
+FROM `t_pasien`
+WHERE DATE(created) = '2021-02-03'
+AND `stts` = 1
+ORDER BY `t_pasien`.`modified` DESC
+ERROR - 2021-02-04 06:49:56 --> Query error: MySQL server has gone away - Invalid query: SELECT *
+FROM `t_pasien`
+WHERE `stts` = 1
+ORDER BY `t_pasien`.`modified` DESC
+ERROR - 2021-02-04 06:49:56 --> Query error: MySQL server has gone away - Invalid query: SELECT *
+FROM `t_berkas`
+ORDER BY `t_berkas`.`modified` DESC
+ERROR - 2021-02-04 06:49:56 --> Query error: MySQL server has gone away - Invalid query: SELECT *
+FROM `t_berkas`
+ORDER BY `t_berkas`.`modified` DESC
+ERROR - 2021-02-04 06:49:56 --> Query error: MySQL server has gone away - Invalid query: SELECT *
+FROM `t_pasien`
+WHERE `stts` = 1
+ORDER BY `t_pasien`.`modified` DESC
+ERROR - 2021-02-04 06:49:56 --> Query error: MySQL server has gone away - Invalid query: SELECT SUM(`stts`) AS `stts`
+FROM `t_pasien`
+WHERE DATE(created) = '2021-02-03'
+AND `stts` = 1
+ORDER BY `t_pasien`.`modified` DESC
